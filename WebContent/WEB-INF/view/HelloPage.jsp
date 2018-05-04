fcf94885c6d77101e89c5bc953ab938d9f3bae2d
nthraj1989 <niitrajnish@gmail.com>
2018-05-04 10:17:27 +0530

    first git commit directly from src

---
+++ WebContent/WEB-INF/view/HelloPage.jsp
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>SpringFirstMVCProject</title>
+</head>
+<body>
+Multi @Request Mapping in one controller class 
+<br><br>
+Message:${msg}
+
+</body>
+</html>
---
+++ src/com/razz/HelloController.java
@@ -0,0 +1,40 @@
+package com.razz;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+//class level request mapping to remove conflicts  
+@RequestMapping("/greet")
+public class HelloController {
+	
+	@RequestMapping("/welcome/{countryName}/{username}")
+	//@Pathvariable used to enter manual URL
+	public  ModelAndView helloworld(@PathVariable("username") String name,
+			@PathVariable("countryName") String countryName) {
+		
+		ModelAndView model= new ModelAndView("HelloPage");
+		model.addObject("msg","hello " +name+""+"you are from "+countryName);
+		return model;
+		
+	}
+	
+	//multiaction controller class
+	
+	@RequestMapping("/{stateName}/{username}")
+	//PathVariable using Map in the method argument
+	public ModelAndView hiworld(@PathVariable Map<String,String> m )
+	{
+		String username=m.get("username");
+		String stateName=m.get("stateName");
+		
+		ModelAndView model= new ModelAndView("HelloPage");
+		model.addObject("msg","Hi "+username+"You belong to state "+stateName);
+		return model;
+	}
+
+}
